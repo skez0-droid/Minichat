b3952289986285e39fa3e701c4931a4a7e6f73bc
pfarbos <paulin.farbos@gmail.com>
2023-04-15 19:57:07 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,47 @@
+
+import socket
+import threading
+from datetime import datetime
+
+clients = []
+pseudo = ''
+
+# fonction exécutée pour recevoir les messages
+def receive_data(connection, address, pseudo):
+	while True:
+    		data = connection.recv(1024)
+   	 	# si un message est reçu
+    		if data:
+        		a = '['+str(datetime.now()) +']'+ pseudo + ' >> ' + data.decode()
+        		print(a)
+   		 	# bouble pour envoyer le message à tous les clients
+        		for client in clients:
+       		 		client.sendall(a.encode())
+
+
+# Fonction exécutée pour la connexion des clients
+def new_client(sock):
+	while True:
+   		 # Attente d'une connexion
+		print('Wait for connexion...')
+		connection, client_address = sock.accept()
+		pseudo = connection.recv(1024)
+		pseudotamp = pseudo.decode()
+		print(client_address,pseudotamp, 'is connected.')
+		threading.Thread(target=receive_data,args=(connection, client_address, pseudo.decode())).start()
+		clients.append(connection)
+# Création d'un socket TCP/IP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Liaison du socket à une adresse et un port
+server_address = ('192.168.1.77', 2261)
+print('démarrage sur %s port %s' % server_address)
+sock.bind(server_address)
+
+# Écoute des connexions entrantes
+sock.listen(1)
+
+# thread pour gerer les connexions
+threading.Thread(target=new_client,args=(sock,)).start()
+
+
