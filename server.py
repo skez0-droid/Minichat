9071c03c22abdac1a8adc18c07c032d277c2f2d4
nilu4p <paulin.farbos@gmail.com>
2024-09-26 15:02:40 +0200

    server.py

--- server.py
+++ server.py
@@ -6,30 +6,6 @@ from datetime import datetime
 clients = []
 pseudo = ''
 
-# fonction exécutée pour recevoir les messages
-def receive_data(connection, address, pseudo):
-	while True:
-    		data = connection.recv(1024)
-   	 	# si un message est reçu
-    		if data:
-        		a = '['+str(datetime.now()) +']'+ pseudo + ' >> ' + data.decode()
-        		print(a)
-   		 	# bouble pour envoyer le message à tous les clients
-        		for client in clients:
-       		 		client.sendall(a.encode())
-
-
-# Fonction exécutée pour la connexion des clients
-def new_client(sock):
-	while True:
-   		 # Attente d'une connexion
-		print('Wait for connexion...')
-		connection, client_address = sock.accept()
-		pseudo = connection.recv(1024)
-		pseudotamp = pseudo.decode()
-		print(client_address,pseudotamp, 'is connected.')
-		threading.Thread(target=receive_data,args=(connection, client_address, pseudo.decode())).start()
-		clients.append(connection)
 # Création d'un socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -43,5 +19,34 @@ sock.listen(1)
 
 # thread pour gerer les connexions
 threading.Thread(target=new_client,args=(sock,)).start()
+
+# Fonction exécutée pour la connexion des clients
+def new_client(sock):
+	while True:
+   		 # Attente d'une connexion
+		print('Wait for connexion...')
+		connection, client_address = sock.accept()
+		pseudo = connection.recv(1024)
+		pseudotamp = pseudo.decode()
+		print(client_address,pseudotamp, 'is connected.')
+		threading.Thread(target=receive_data,args=(connection, client_address, pseudo.decode())).start()
+		clients.append(connection)
+
+		
+# fonction exécutée pour recevoir les messages
+def receive_data(connection, address, pseudo):
+	while True:
+    		data = connection.recv(1024)
+   	 	# si un message est reçu
+    		if data:
+        		a = '['+str(datetime.now()) +']'+ pseudo + ' >> ' + data.decode()
+        		print(a)
+   		 	# boucle pour envoyer le message à tous les clients
+        		for client in clients:
+       		 		client.sendall(a.encode())
+
+
+
+
 
 
